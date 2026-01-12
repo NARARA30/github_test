86356cd67c2c1af362513cb65b451b6581d94e0f
NARARA30 <nara.makne0@gmail.com>
2026-01-12 14:44:44 +0900

    조건문 변경사항 반영

--- if.py
+++ if.py
@@ -12,3 +12,20 @@ print(3==4 or 4<=5 or 6<2)
 
 print(not 3==4)
 
+i=1
+
+if i == 1:
+    print(i)
+
+else:
+    print(i+1)
+
+score = 78
+
+if score>=90:
+    print("a")
+elif score>=75:
+    print('b')
+else:
+    print('c')
+    
